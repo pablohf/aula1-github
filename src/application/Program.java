32050285063464a3c475fd9bdffc41aa1f007dec
Pablo Ferreira <pablo.henrikef@gmail.com>
2019-06-29 13:01:18 -0300

    Projeto Criado

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Hello world!");
+		
+		sc.close();
+
+	}
+
+}
